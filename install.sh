78cee0269f38fb518b901c448837d95a3f457ec0
Vffgggg <deshifarmer295+1@gmail.com>
2024-10-09 00:43:17 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,7 @@ cat <<EOL > .devcontainer/devcontainer.json
             "version": "latest"
         }
     },
-    "postStartCommand": "python3 /workspaces/Ra/titan.py",
+    "postStartCommand": "python3 /workspaces/Sa/titan.py",
     "customizations": {
         "vscode": {
             "settings": {
